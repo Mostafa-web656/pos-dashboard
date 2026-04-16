8936b6a236697024924958be72708a91b90772a3
Mostafa-web656 <mostafasaeed9990@gmail.com>
2026-04-16 21:28:58 +0200

    frontend clean setup

--- branches/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-from core.models import Company
-
-
-class Branch(models.Model):
-    company = models.ForeignKey(Company, on_delete=models.CASCADE)
-    name = models.CharField(max_length=255)
--- pos_core/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def full_page(request, exception=None):
-    return render(request, '404.html')
