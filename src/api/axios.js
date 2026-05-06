6d284f8bb00bc9b75a643a9d2e3034a64c4e8280
Mostafa-web656 <mostafasaeed9990@gmail.com>
2026-05-06 17:12:52 +0300

    fix invoice detail with items

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,20 +2,19 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://mostafasaeed.pythonanywhere.com/api/",
-  headers: {
-    "Content-Type": "application/json",
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("access");
+
+    if (token) {
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+
+    return config;
   },
-});
-
-// 🔥 FIX FINAL
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
-});
+  (error) => Promise.reject(error)
+);
 
 export default api;
